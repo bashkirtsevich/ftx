fac5fe4bdb96b777fef039ea69b923422f07f5c7
bashkirtsevich <bashkirtsevich@gmail.com>
2025-10-19 00:15:02 +0500

    Small code restructurization in `msk_gen_signal`

--- msk.py
+++ msk.py
@@ -17,14 +17,14 @@ def msk_gen_signal(
         freq = carrier_freq + tone * delta_freq
         phase_delta = 2 * np.pi * freq * dt
 
-        t_start = i * samples_per_symbol
-        t_end = t_start + samples_per_symbol
-
         phases = np.fromiter(
             (np.fmod(phase_delta * i + phase, 2 * np.pi)
              for i in range(samples_per_symbol)),
             dtype=np.float64
         )
+
+        t_start = i * samples_per_symbol
+        t_end = t_start + samples_per_symbol
 
         signal[t_start:t_end] = np.sin(phases)
 
