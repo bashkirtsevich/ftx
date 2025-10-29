dd8d41520e13db02abc563730703688fc06ceaaa
bashkirtsevich <bashkirtsevich@gmail.com>
2025-10-29 23:38:06 +0500

    Replace osd to ldpc folder

--- decoders/ftx.py
+++ decoders/ftx.py
@@ -9,7 +9,7 @@ from crc.ftx import ftx_extract_crc, ftx_check_crc
 from encoders import ft4_encode, ft8_encode
 from ldpc.ftx import bp_decode
 from message import message_decode
-from osd import osd_decode
+from ldpc.osd import osd_decode
 from monitor import DecodeStatus, AbstractMonitor
 
 
